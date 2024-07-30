620085811600fba38029f00b34811060f457ab2a
J S Mammen <jsmammen@gmail.com>
2024-07-30 08:25:23 +0100

    Added Job component

--- src/app/components/Jobs.tsx
+++ src/app/components/Jobs.tsx
@@ -1,15 +1,20 @@
-export default function Jobs({ header, jobs }: { header: string, jobs: Job[] }) {
+export default function Jobs({ header }: { header: string }) {
     return (
         <div className="bg-slate-200 py-6 rounded-3xl">
             <div className="container">
                 <h2 className="font-bold mb-4">{header || 'Recent jobs'}</h2>
 
                 <div className="flex flex-col gap-4">
-
-                    <div>No jobs found</div>
-
+                    <div className="bg-white p-6 rounded-sm shadow-sm">
+                        Test job 1
+                    </div>
+                    <div className="bg-white p-6 rounded-sm shadow-sm">
+                        Test job 1
+                    </div>
+                    <div className="bg-white p-6 rounded-sm shadow-sm">
+                        Test job 1
+                    </div>
                 </div>
-
             </div>
         </div>
     );
